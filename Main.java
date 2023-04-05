6fc5d2047382d70ee8e2677f898fb2e15d3dbffe
Reema <129707333+ReemaMQ@users.noreply.github.com>
2023-04-05 04:50:15 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,56 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+        //   1 <<<Print the numbers from 1-100, then print "The number is even">>> done
+
+//        for (int i = 1; i <=100 ; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println(i + " : The number is even");
+//            } else {
+//               } System.out.println(i);
+//            }
+
+//          2 <<<Print the numbers from 1-10 Stop repeating if the number is (4)>>> done
+
+//        int num = 0;
+//        for (int i = 0; i < 10; i++) {
+//            if (i == 4) {
+//                break;
+//            }
+//            System.out.println(i);
+//        }
+
+//       3 <<<Print only odd numbers from 1-100 (Use continue)>>> done
+
+//        for (int i = 1; i <=100; i++) {
+//            if (i%2==0){
+//                continue;// skip even numbers
+//            }
+//            System.out.println(i);// print odd numbers
+//
+//        }
+
+//         4 <<<text value with my name, Print each letter on a line>>>done
+//
+//        String My_Name = "REEMA";
+//        for (int i = 0; i < My_Name.length(); i++) {
+//            System.out.println(My_Name.charAt(i));
+//
+//        }
+
+//         5 <<<text value with my name Print each character in a line backwards>>> done
+//
+//        String MyName = "REEMA";
+//        for (int i = MyName.length() - 1; i >= 0; i--) {
+//            System.out.println(MyName.charAt(i));
+//
+//        }
+
+
+
+    }}
+
+
